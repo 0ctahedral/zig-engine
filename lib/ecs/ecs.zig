ee9345e0ea27fe7760f63be1b95c9ccfd51f1e46
xenone <rosenkranz1999@gmail.com>
2021-08-11 22:48:32 -0400

    halfway to figuring it out

--- lib/ecs/ecs.zig
+++ lib/ecs/ecs.zig
@@ -1,11 +1,31 @@
 const std = @import("std");
-const testing = std.testing;
 
 const max_components = 1000;
 
 const EntityId = usize;
 // id 0 is reserved for no entity
 const nullEntity: EntityID = 0;
+
+// start
+// indicies 0 1 2 3 4 5 6 7 8 9 
+// slots   |_|_|_|_|_|_|_|_|_|_|
+// ids     |0|1|2|3|4|5|6|7|8|9|
+// ptr      ^
+// add(id = 3)
+// indicies 0 1 2 3 4 5 6 7 8 9 
+// slots   |C|_|_|_|_|_|_|_|_|_|
+// ids     |3|1|2|0|4|5|6|7|8|9|
+// ptr        ^
+// add(id = 2)
+// indicies 0 1 2 3 4 5 6 7 8 9 
+// slots   |C|C|_|_|_|_|_|_|_|_|
+// ids     |3|2|1|0|4|5|6|7|8|9|
+// ptr          ^
+// add(id = 4)
+// indicies 0 1 2 3 4 5 6 7 8 9 
+// slots   |C|C|C|_|_|_|_|_|_|_|
+// ids     |3|2|4|0|1|5|6|7|8|9|
+// ptr            ^
 
 pub fn ComponentStore(
     comptime T: type,
@@ -14,36 +34,81 @@ pub fn ComponentStore(
 
         allocator: *std.mem.Allocator,
         // TODO: should this reallocate?
-        items:  []align(1)T,
+        items:  []T,
 
-        idToIndex: [max_components]usize,
-        indexToId: [max_components]usize,
+        idToIndex: []usize,
+        indexToId: []usize,
+
+        next_idx: usize = 0,
 
         const Self = @This();
 
+        /// Create the store
         pub fn init(allocator: *std.mem.Allocator) !Self {
-            return Self {
+            var ret = Self {
                 .allocator = allocator,
-                .items = try allocator.alloc(T, max_components * @sizeOf(T)),
+                .items = try allocator.alloc(T, max_components),
+                .idToIndex = try allocator.alloc(usize, max_components),
+                .indexToId = try allocator.alloc(usize, max_components),
             };
+
+            std.mem.set(T, ret.items, .{});
+            std.mem.set(usize, ret.idToIndex, 0);
+            std.mem.set(usize, ret.indexToId, 0);
+
+            return ret;
         }
 
         /// Add a component 
         /// replaces component if it already exists
-        pub fn add(c: T, id: usize) !void {
-            if (id > max_components) 
+        pub fn add(self: *Self, id: usize, c: T) !void {
+            if (id > max_components or id < 1) 
                 return error.InvalidId;
-            // add it
-            items[indexToId] = c;
+
+            const index = self.idToIndex[id];
+
+            // we already have this entity, replacing
+            if (index > 0) {
+                self.items[index] = c;
+                return;
+            }
+            // put em in at the end
+            self.indexToId[self.next_idx] = id;
+            self.idToIndex[id] = self.next_idx;
+
+            self.next_idx += 1;
         }
 
         /// Remove component 
-        pub fn remove(id: usize) !void {
+        pub fn remove(self: *Self, id: usize) !void {
+            if (id > max_components or id < 1) 
+                return error.InvalidId;
 
+            // do we have a component
+            const index = self.idToIndex[id];
+
+            // we already have this entity, replacing
+            if (index < 1) {
+                return error.NoComponent;
+            }
+
+            self.next_idx -= 1;
+            // take value from the last idx and switch em
+            self.items[index] = self.items[self.next_idx];
+            // id of last item in array
+            const l_id = self.indexToId[self.next_idx];
+            // set new index
+            self.idToIndex[l_id] = index;
+
+            // clear
+            self.idToIndex[id] = 0;
+            self.indexToId[self.next_idx] = 0;
         }
 
         pub fn deinit(self: *Self) void {
-            self.allocator.free(self.items.ptr[0..max_components]);
+            self.allocator.free(self.items);
+            self.allocator.free(self.idToIndex);
+            self.allocator.free(self.indexToId);
         }
 
     };
--- lib/ecs/ecs_test.zig
+++ lib/ecs/ecs_test.zig
@@ -3,12 +3,61 @@ const ecs = @import("ecs.zig");
 const testing = std.testing;
 
 const vec3 = struct {
-    x: f32,
-    y: f32,
-    z: f32,
+    x: f32 = 0,
+    y: f32 = 0,
+    z: f32 = 0,
 };
 
 test "component store" {
-    var cs = try ecs.ComponentStore(vec3).init(std.heap.page_allocator);
-    defer cs.deinit();
+    var vec_store = try ecs.ComponentStore(vec3).init(std.testing.allocator);
+    defer vec_store.deinit();
+}
+
+test "add component" {
+    var vec_store = try ecs.ComponentStore(vec3).init(std.testing.allocator);
+    defer vec_store.deinit();
+
+    try vec_store.add(@as(usize, 3), .{.x=1, .y=2, .z=3});
+
+    // id should increase
+    try testing.expect(vec_store.next_idx == 1);
+    // first index should be 0
+    try testing.expect(vec_store.indexToId[0] == 3);
+    try testing.expect(vec_store.idToIndex[3] == 0);
+
+    // test adding another
+    try vec_store.add(@as(usize, 2), .{.x=4, .y=5, .z=6});
+    try testing.expect(vec_store.next_idx == 2);
+    try testing.expect(vec_store.idToIndex[2] == 1);
+    try testing.expect(vec_store.indexToId[1] == 2);
+
+    // shouldn't be able to do null entity
+    try testing.expectError(error.InvalidId, vec_store.add(@as(usize, 0), .{}));
+}
+
+test "remove component" {
+    var vec_store = try ecs.ComponentStore(vec3).init(std.testing.allocator);
+    defer vec_store.deinit();
+
+    // can't do anything with a null entity
+    try testing.expectError(error.InvalidId, vec_store.remove(@as(usize, 0)));
+
+    // can't remove from entity without component
+    try testing.expectError(error.NoComponent, vec_store.remove(@as(usize, 2)));
+
+    // add a few
+    try vec_store.add(@as(usize, 1), .{.x=1, .y=2, .z=3});
+    try vec_store.add(@as(usize, 2), .{.x=1, .y=2, .z=3});
+    try vec_store.add(@as(usize, 3), .{.x=1, .y=2, .z=3});
+
+    // remove the first one
+    try vec_store.remove(@as(usize, 1));
+    // this means that the index should move the last index back
+    // and that the indicies should have been swapped
+    // | 1 | 2 | 3 | tmp = 3
+    // | _ | 2 | 3 | [0] = tmp
+    // | 3 | 2 | _ | clear 3
+    //try testing.expect(vec_store.idToIndex[1] == 0);
+    //try testing.expect(vec_store.idToIndex[2] == 2);
+    //try testing.expect(vec_store.idToIndex[3] == 1);
 }
